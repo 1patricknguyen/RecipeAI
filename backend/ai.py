e67da53d351b1cf217e551c162dc104fdee6aa43
1patricknguyen <patricknguyen@ufl.edu>
2024-07-31 00:31:24 -0400

    Added schema checks in case AI provides faulty json objects

--- backend/ai.py
+++ backend/ai.py
@@ -13,14 +13,22 @@ model = genai.GenerativeModel('gemini-1.5-flash', generation_config={"response_m
 def create_recipe(ingredients):
     ingredients_list = ingredients["ingredients"]
     prompt = f'''Make me a recipe with only these ingredients I list: {ingredients_list}. Assume I have pantry spices.
-    Return this JSON schema: {{"title": string, "ingredients": list[Name: item], "steps": list[steps]}}'''
+    Return this JSON schema: {{"title": string, "ingredients": list[Name: string (with quantities)], "steps": list[steps]}}'''
     response = model.generate_content(prompt)
     return response.text
 
 
-test = {"ingredients": "ribeye, potatoes, mushrooms, broccoli"}
-print(create_recipe(test))
 
+def link_recipe(ingredients):
+    ingredients_list = ingredients["ingredients"]
+    prompt = f'''Give me links to online recipes with these ingredients I list: {ingredients_list}. Assume I have pantry spices. Additionally, these
+    recipes could have additional ingredients but try to keep it mostly within the list.
+    Return this JSON schema {{"links: [{{"title": "string", "link": "URL"}}]}}
+    Maximum 5 links.'''
+    response = model.generate_content(prompt)
+    return response.text
 
+ing = {"ingredients": "chicken, rice, soy sauce, garlic, ginger, green onions, sesame oil, sugar, cornstarch, vinegar, salt, pepper, oil"}
+print(create_recipe(ing))
                                                                        
 
--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,65 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from jsonschema import validate, ValidationError, SchemaError
 import json
 import ai
 
 
 app = Flask(__name__)
 CORS(app)
+
+recipe_schema = {
+    "type": "object",
+    "properties": {
+        "title": {"type": "string"},
+        "ingredients": {"type": "array", "items": {"type": "string"}},
+        "steps": {"type": "array", "items": {"type": "string"}}
+    },
+    "required": ["title", "ingredients", "steps"]
+}
+
+links_schema = {
+    "type": "object",
+    "properties": {
+        "links": {"type": "array", "items": {"type": "object", "properties": {"title": {"type": "string"}, "link": {"type": "string"}}}}
+    },
+    "required": ["links"]
+}
+
+
 @app.route('/api/recipe', methods=['POST'])
 def generate_recipe():
     data = request.get_json()
     result = json.loads((ai.create_recipe(data)))
-    return jsonify(result)
+    try:
+        validate(instance=result, schema=recipe_schema)
+        return jsonify(result), 200
+    
+    except ValidationError as e:
+        return jsonify({"message": "Validation Error", "error": e.message}), 400
+    
+    except SchemaError as e:
+        return jsonify({"message": "Schema Error", "error": e.message}), 500
+    
+    except Exception as e:
+        return jsonify({"message": "Other Error", "error": str(e)}), 500
+
+
+@app.route('/api/link', methods=['POST'])
+def generate_link():
+    data = request.get_json()
+    result = json.loads((ai.link_recipe(data)))
+    try:
+        validate(instance=result, schema=links_schema)
+        return jsonify(result), 200
+    
+    except ValidationError as e:
+        return jsonify({"error": e.message}), 400
+    
+    except SchemaError as e:
+        return jsonify({"error": e.message}), 500
+    
+    except Exception as e: 
+        return jsonify({"error": str(e)}), 500
+    
     
